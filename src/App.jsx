096bb301b812cd5cd9b6652c4f2bba585fd9a740
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-02-10 08:55:46 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+    <main>
+      <h1>¡Hola, Mi nombre es Luis Alejandro Londoño Valle!</h1>
+      <hr />
+      <p>Soy un joven de 19 años con conocimientos en C# y .NET, y un marcado interés en especializarme en desarrollo de software. Actualmente curso Tecnología en Análisis y Desarrollo de Software en el SENA y Ingeniería de Software en el Tecnológico de Antioquia, con el objetivo de adquirir continuamente nuevos conocimientos y experiencias que me permitan crecer en el ámbito tecnológico. Me caracterizo por ser una persona solidaria, trabajadora y responsable, comprometida con la búsqueda constante de oportunidades para mejorar mis habilidades y contribuir de manera significativa en proyectos innovadores.</p>
+    </main>
+    
+    <footer className="footer">
+        <div className="footer-container">
+          <p id="copyright">© 2025 Luis Alejandro Londoño Valle · Construyendo backends impecables</p>
+
+          <div className="footer-links">
+            <a
+              href="https://github.com/AlejandroLondonoValle"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://co.linkedin.com/in/lu%C3%ADs-alejandro-londo%C3%B1o-valle"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+            <a href="mailto:londonovalleluisalejandro@gmail.com">
+              Email
+            </a>
+          </div>
+        </div>
+      </footer>
+
+    </>
+
+  );
+}
+
