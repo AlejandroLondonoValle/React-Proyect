142a3b4d78b3544770581ff71b3e41fa07171a21
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-02-24 11:36:00 -0500

    Clase_7_Agenda_ADSO_v5_API

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 export default {
   plugins: {
+    // Registra Tailwind como plugin de PostCSS
     tailwindcss: {},
+    
+    // Autoprefixer agrega compatibilidad de prefijos CSS
     autoprefixer: {},
   },
-}
+};
